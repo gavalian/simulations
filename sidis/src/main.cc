152cc0ebf6ae32d9d80f7290168056845d9ddb60
gavalian <gavalian@jlab.org>
2025-05-01 13:56:59 -0400

    updated the code to calculate phi dependent W functions

--- sidis/src/main.cc
+++ sidis/src/main.cc
@@ -10,8 +10,13 @@ int main()
     double kap = 0.3;            //   φ  in radians
 
     u[h('0')][h('0')][h('+')][h('+')] = {0.0123, 0.0000};   // real example
+    u[h('0')][h('0')][h('0')][h('+')] = {0.0150, 0.0240};   // real example
     l[h('+')][h('-')][h('0')][h('+')] = {0.0,    -0.0071};  // imaginary
     
-    auto wUU = UU(u, eps, phi, kap);
-    std::cout << "W_UU^LL = " << wUU.LL << '\n';
+    for(double phi2 = -3.14; phi2<3.14; phi2+=0.2){
+        phi = phi2;
+        auto wUU = UU(u, eps, phi, kap);
+        std::cout <<  "PHI = " <<  phi << "  W_UU^LL = " << wUU.LL << '\n';
+    }
+
 }
