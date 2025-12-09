f00d3358c9ce5def01b34c82d09bc0a21877712e
gavalian <gavalian@jlab.org>
2025-12-09 10:02:51 -0500

    fixed the phiproduce script

--- sidis/src.n/phiproduce.C
+++ sidis/src.n/phiproduce.C
@@ -3,20 +3,24 @@
 void phiproduce(){
   // define a reaction to decay to proton and rho
   // where rho decays to two pions
-  //sim::candidate cr(10.6,0.77,0.13957,0.13957);
+
   sim::candidate cr(10.6,1.02,0.49368,0.49368);
   sim::generator gen(&cr);
   sim::event     event;
-
-  cr.react.setDecayIds(333,321,-321);
   
+  cr.react.setDecayIds(333,321,-321);
   // set range for generation, Q2 min, Q2 max, Xb min, Xb max
-  gen.setRange(1.5,2.5,0.05,1.0);
-
-  for(int j = 0; j < 24; j++) {
-    gen.generate();
+  gen.setRange(2.45,2.5,0.35,0.36);
+  double weight = gen.scan(150000);
+  
+  for(int j = 0; j < 24000; j++) {
+    gen.generate(weight);
     //cr.react.show();
     cr.react.getEvent(event);
+    // printf(" %d\n", event.beamPol);
     event.show();
   }
+
+  //gen.stats();
+  
 }
